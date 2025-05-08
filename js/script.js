fd18ebdca843c39aceca81b35379e2b675e5d631
CitlaliMartinez08 <144378895+CitlaliMartinez08@users.noreply.github.com>
2025-05-08 09:24:37 -0600

    prueba1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.202.207.92/api/devices";
+const API_URL = "http://3.238.186.115/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
