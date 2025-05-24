f9cd81fbc14359b44f963125a5a9cb6624107683
CitlaliMartinez08 <144378895+CitlaliMartinez08@users.noreply.github.com>
2025-05-23 19:50:10 -0600

    carrito

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.193.15.36/api/devices";
+const API_URL = "http://44.222.83.134/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
