5e2f4e666983fc7a3477451a08e8085a8fe0878d
CitlaliMartinez08 <144378895+CitlaliMartinez08@users.noreply.github.com>
2025-05-26 11:04:25 -0600

    clase2605

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.222.83.134/api/devices";
+const API_URL = "http://18.215.186.40/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
